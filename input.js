bfdc854da90d5174aac300979bb3c00acaede85f
hoshaun <shaun.ho@mail.utoronto.ca>
2023-03-15 14:16:14 -0400

    add WASD movement

--- input.js
+++ input.js
@@ -1,13 +1,32 @@
-const setupInput = function() {
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", handleUserInput);
+  stdin.on("data", (key) => {
+    connection.write(`Move: ${handleUserInput(key)}`);
+    connection.write('Name: SHO');
+  });
   return stdin;
 };
 
 const handleUserInput = function(key) {
+  if (key === 'w') {
+    return 'up';
+  }
+  if (key === 'a') {
+    return 'left';
+  }
+  if (key === 's') {
+    return 'down';
+  }
+  if (key === 'd') {
+    return 'right';
+  }
   if (key === '\u0003') {
     process.exit();
   }
